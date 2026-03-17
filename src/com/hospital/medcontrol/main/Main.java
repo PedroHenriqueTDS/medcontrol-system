971de00a16e007c7b045fb76249718029222139a
Pedro Henrique Trajano da Silva <pedroht1p7@gmail.com>
2026-03-17 14:49:36 -0300

    Adicionando "S"

--- src/com/hospital/medcontrol/main/Main.java
+++ src/com/hospital/medcontrol/main/Main.java
@@ -2,6 +2,6 @@ package com.hospital.medcontrol.main;
 
 public class Main {
     static void main(String[] args) {
-        System.out.println("Está funcionando, Vaso!");
+        System.out.println("Está funcionando, Vasos!");
     }
 }
